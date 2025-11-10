55eb4377d61c637a8cc0159ed33d8249ac9531df
BlanchardIlela <blancilela@gmail.com>
2025-11-10 06:20:42 +0100

    ajout des colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(var(--color-primary) / <alpha-value>)",
+        secondary: "hsl(var(--color-secondary) / <alpha-value>)",
+        accent: "hsl(var(--color-accent) / <alpha-value>)",
+        text: "hsl(var(--color-text) / <alpha-value>)",
+      }
+    },
   },
   plugins: [],
 }
